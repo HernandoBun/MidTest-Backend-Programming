65f5d9347e08f98485045e5424a1f2fffb84a582
HernandoBun <hernandobun@gmail.com>
2024-04-25 12:21:54 +0700

    menyederhanakan users data karena output terdapat password

--- src/api/components/users/users-repository.js
+++ src/api/components/users/users-repository.js
@@ -11,10 +11,18 @@ async function getUsers(noPage = 1, ukPage = 10, sort = { email: 1 }, search = {
                           .skip(skip)
                           .limit(ukPage);
 
-  const count = await User.countDocuments(search); // hitung pages
-  const totalPages = Math.ceil(count / ukPage); // total pages
-  const pageSebelum = noPage > 1;  // page sebelum apakah ada? true/false
-  const pageSetelah = noPage < totalPages; // page sesudha apakah ada? true/false 
+  const count = await User.countDocuments(search); // hitung jumlah data
+  const totalPages = Math.ceil(count / ukPage); // total halaman
+  const pageSebelum = noPage > 1; // apakah halaman sebelumnya ada? true/ false
+  const pageSetelah = noPage < totalPages; // apakah halaman selanjutnya ada? true / false
+
+  // Menyederhanakan setiap objek pengguna untuk hanya mengandung id, nama, dan email
+  // karena code outputnya ada password
+  const usersData = users.map(user => ({
+    id: user._id,
+    name: user.name,
+    email: user.email
+  }));
 
   return {
     page_number: noPage,
@@ -23,9 +31,10 @@ async function getUsers(noPage = 1, ukPage = 10, sort = { email: 1 }, search = {
     total_pages: totalPages,
     has_previous_page: pageSebelum,
     has_next_page: pageSetelah,
-    data: users
+    data: usersData
   };
 }
+
 
 /**
  * Get user detail
--- src/api/components/users/users-service.js
+++ src/api/components/users/users-service.js
@@ -10,20 +10,22 @@ async function getUsers(pageNumber, pageSize, sort, search) {
   // Parsing query
   const page_number = parseInt(pageNumber) || 1;
   const page_size = parseInt(pageSize) || 10;
-  const sortParams = parseSortParams(sort);
-  const searchParams = parseSearchParams(search);
+  const sortParams = parseSortFunction(sort);
+  const searchParams = parseSearchFunction(search);
 
   return await usersRepository.getUsers(page_number, page_size, sortParams, searchParams);
 }
 
-function parseSortParams(sortQuery) {
-  if (!sortQuery) return { email: 1 }; // Default sorting by email ascending
+function parseSortFunction(sortQuery) {
+  if (!sortQuery) 
+    return { email: 1 }; //  sorting email ascending
   const [field, order] = sortQuery.split(':');
   return { [field]: order === 'desc' ? -1 : 1 };
 }
 
-function parseSearchParams(searchQuery) {
-  if (!searchQuery) return {};
+function parseSearchFunction(searchQuery) {
+  if (!searchQuery) 
+    return {};
   const [field, key] = searchQuery.split(':');
   return { [field]: { $regex: key, $options: 'i' } }; // Case insensitive search
 }
